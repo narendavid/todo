a0f256704d02e191786352470e4a6dcb07c63c81
Naren Luna <narenkun96@gmail.com>
2023-11-15 12:15:12 -0500

    integrate nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@nextui-org/react";
+
 const App = () => {
     return (
         <h1 className="text-3xl font-bold underline">
             Hello world!
+            <Button color="primary">
+                Button
+            </Button>
         </h1>
     )
 }
